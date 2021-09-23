5a9c5f5fd3079730e7d5c1efaec077cc38963d29
shimpen <shimpenguin@gmail.com>
2021-09-24 01:21:27 +0900

    choir: down get limits temporaly

--- src/domains/pokeapi/data/region-url-list.ts
+++ src/domains/pokeapi/data/region-url-list.ts
@@ -7,15 +7,28 @@ export type RegionUrlMap = { [key in Region]: Url };
 const baseUrl = `https://pokeapi.co/api/v2/pokemon?`;
 
 const regionUrlMap: RegionUrlMap = {
-  all: `${baseUrl}limit=100&offset=0`,
-  kanto: `${baseUrl}limit=151&offset=0`,
-  johto: `${baseUrl}limit=100&offset=151`,
-  hoenn: `${baseUrl}limit=135&offset=251`,
-  sinnoh: `${baseUrl}limit=97&offset=386`,
-  unova: `${baseUrl}limit=156&offset=493`,
-  kalos: `${baseUrl}limit=72&offset=649`,
-  alola: `${baseUrl}limit=88&offset=721`,
-  galar: `${baseUrl}limit=89&offset=809`,
+  all: `${baseUrl}limit=20&offset=0`,
+  kanto: `${baseUrl}limit=20&offset=0`,
+  johto: `${baseUrl}limit=20&offset=151`,
+  hoenn: `${baseUrl}limit=20&offset=251`,
+  sinnoh: `${baseUrl}limit=20&offset=386`,
+  unova: `${baseUrl}limit=20&offset=493`,
+  kalos: `${baseUrl}limit=20&offset=649`,
+  alola: `${baseUrl}limit=20&offset=721`,
+  galar: `${baseUrl}limit=20&offset=809`,
 } as const;
+
+// Fixme: add see more function and remove under comments that get all each region.
+// const regionUrlMap: RegionUrlMap = {
+//   all: `${baseUrl}limit=100&offset=0`,
+//   kanto: `${baseUrl}limit=151&offset=0`,
+//   johto: `${baseUrl}limit=100&offset=151`,
+//   hoenn: `${baseUrl}limit=135&offset=251`,
+//   sinnoh: `${baseUrl}limit=97&offset=386`,
+//   unova: `${baseUrl}limit=156&offset=493`,
+//   kalos: `${baseUrl}limit=72&offset=649`,
+//   alola: `${baseUrl}limit=88&offset=721`,
+//   galar: `${baseUrl}limit=89&offset=809`,
+// } as const;
 
 export default regionUrlMap;
